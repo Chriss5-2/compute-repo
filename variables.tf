8330298a6ddefce50ad70950d0bf0de10159c31b
Chriss5-2 <christiangiovannixd@gmail.com>
2025-06-13 13:38:48 -0500

    Add instance_count as a variable with default number=2

--- variables.tf
+++ variables.tf
@@ -6,3 +6,10 @@ variable "name" {
 variable "network_name" {
   type        = string
 }
+
+# Agregando instance_count
+variable "instance_count" {
+  description = "Number of instaces"
+  type    = number
+  default = 2
+}
